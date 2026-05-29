43f642f4980d0df7f4c60d4bc00fb4dd750f8606
felipe_cruz <felipe.sousa@biomtech.com.br>
2026-05-29 15:22:06 -0300

    Inclusão multi-thread simulation

--- src/main.py
+++ src/main.py
@@ -1,17 +1,13 @@
 from src.usecases.simulateIoTUsecase import SimulateIoTUsecase
-import uvicorn
+
+import pandas as pd
 import threading
+import queue
+import uvicorn
 import time
 
-
-def run_simulation():
-    path = "assets/50_rows_simulate.xlsx"
-    simulate_iot_usecase = SimulateIoTUsecase(path)
-    simulate_iot_usecase.verifyHealth()
-    simulate_iot_usecase.simulate()
-
-
 def start_api():
+
     uvicorn.run(
         "src.api.server:app",
         host="0.0.0.0",
@@ -19,8 +15,25 @@ def start_api():
         reload=False
     )
 
+def worker(queue, simulate_usecase):
+
+    while not queue.empty():
+
+        try:
+
+            item = queue.get()
+
+            simulate_usecase.simulate(item)
+
+            queue.task_done()
+
+        except Exception as e:
+            print(e)
 
 def main():
+
+    #Worker de API em paralelo
+    path = "assets/50_rows_simulate.xlsx"
 
     api_thread = threading.Thread(
         target=start_api,
@@ -29,10 +42,34 @@ def main():
 
     api_thread.start()
 
-    time.sleep(2)
+    time.sleep(3)
 
-    run_simulation()
+    simulate_usecase = SimulateIoTUsecase()
 
+    data = simulate_usecase.load_excel(path)
+
+    q = queue.Queue()
+
+    for item in data:
+        q.put(item)
+
+    threads = []
+
+    num_threads = 10
+
+    for _ in range(num_threads):
+
+        t = threading.Thread(
+            target=worker,
+            args=(q, simulate_usecase)
+        )
+
+        t.start()
+
+        threads.append(t)
+
+    for t in threads:
+        t.join()
 
 if __name__ == "__main__":
     main()
--- src/usecases/simulateIoTUsecase.py
+++ src/usecases/simulateIoTUsecase.py
@@ -1,53 +1,44 @@
 import pandas as pd
-import requests as req
+import requests
+
 from src.models.models import IoTData
-import time
-import json
 
 class SimulateIoTUsecase:
-    def __init__(self, repository_path):
-        self.repository_path = repository_path
-        self.data = None
-        try: 
-            df = pd.read_excel(repository_path)
-            self.data = df
-        except Exception as e:
-            print(f"Erro ao ler o arquivo de dados IoT: {e}")
-    def simulate(self):
-        if self.data is None:
-            print("Nenhum dado disponível para simulação")
-            return
-        
-        for _, row in self.data.iterrows():
 
-            data = IoTData(
-                log_id = row['log_id'],
-                farm_id = row['farm_id'],
-                farm_region = row['farm_region'],
-                sensor_id = row['sensor_id'],
-                device_type = row['device_type'],
-                failure_category = row['failure_category'],
-                failure_timestamp = row['failure_timestamp'],
-                downtime_hours = row['downtime_hours'],
-                resolution_action = row['resolution_action'],
-                temperature_celsius = row['temperature_celsius'],
-                humidity_percent = row['humidity_percent'],
-                weather_condition = row['weather_condition'],
-                soil_moisture_percent = row['soil_moisture_percent'],
-                maintenance_team = row['maintenance_team'],
-                resolved = row['resolved'],
-                estimated_loss_usd = row['estimated_loss_usd']
-            )
+    def __init__(self):
 
-            response = req.post("http://localhost:8000/send-iot-data", json = data.dict())
-            if response.status_code != 200:
-                raise Exception(f"Erro ao enviar dados: {response.status_code} - {response.text}")
-    def verifyHealth(self):
-        while True:
-            response = req.get("http://localhost:8000/health")
-            if response.status_code == 200:
-                break
-            else:
-                time.sleep(2)
-                self.verifyHealth()
-            
+        self.session = requests.Session()
+
+    def load_excel(self, path):
+
+        df = pd.read_excel(path)
+
+        return df.to_dict(orient="records")
+
+    def simulate(self, item):
+
+        data = IoTData(
+            log_id=item['log_id'],
+            farm_id=item['farm_id'],
+            farm_region=item['farm_region'],
+            sensor_id=item['sensor_id'],
+            device_type=item['device_type'],
+            failure_category=item['failure_category'],
+            failure_timestamp=item['failure_timestamp'],
+            downtime_hours=item['downtime_hours'],
+            resolution_action=item['resolution_action'],
+            temperature_celsius=item['temperature_celsius'],
+            humidity_percent=item['humidity_percent'],
+            weather_condition=item['weather_condition'],
+            soil_moisture_percent=item['soil_moisture_percent'],
+            maintenance_team=item['maintenance_team'],
+            resolved=item['resolved'],
+            estimated_loss_usd=item['estimated_loss_usd']
+        )
+
+        response = self.session.post(
+            "http://localhost:8000/send-iot-data",
+            json=data.model_dump()
+        )
+
+        print(f"{response.status_code}\n")
